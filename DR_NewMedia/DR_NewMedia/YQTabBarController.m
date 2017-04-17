5ab95c2dc69ad88b56ed972f65d96c361115d397
杨庆庆 <972544908@qq.com>
2017-04-17 15:52:53 +0800

    newMedia_init

---
+++ DR_NewMedia/DR_NewMedia/YQTabBarController.m
@@ -0,0 +1,29 @@
+//
+//  YQTabBarController.m
+//  DR_NewMedia
+//
+//  Created by 杨庆 on 2017/4/17.
+//  Copyright © 2017年 YQ. All rights reserved.
+//
+
+#import "YQTabBarController.h"
+
+@interface YQTabBarController ()
+
+@end
+
+@implementation YQTabBarController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+@end
