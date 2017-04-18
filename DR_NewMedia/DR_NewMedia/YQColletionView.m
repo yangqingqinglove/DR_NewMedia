26674f201ba8c08fb4cb8be4bbdc73704904b4e7
杨庆庆 <972544908@qq.com>
2017-04-18 09:19:43 +0800

    NewMedia_init_framework

---
+++ DR_NewMedia/DR_NewMedia/YQColletionView.m
@@ -0,0 +1,21 @@
+//
+//  YQColletionView.m
+//  DR_NewMedia
+//
+//  Created by 杨庆 on 2017/4/17.
+//  Copyright © 2017年 YQ. All rights reserved.
+//
+
+#import "YQColletionView.h"
+
+@implementation YQColletionView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
---
+++ DR_NewMedia/DR_NewMedia/YQHomeViewController.m
@@ -0,0 +1,28 @@
+//
+//  YQHomeViewController.m
+//  DR_NewMedia
+//
+//  Created by 杨庆 on 2017/4/17.
+//  Copyright © 2017年 YQ. All rights reserved.
+//
+
+#import "YQHomeViewController.h"
+
+@interface YQHomeViewController ()
+
+@end
+
+@implementation YQHomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
