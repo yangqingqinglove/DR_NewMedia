64ff6494a714ed8cc827bee68e7e5a2bd6629ad3
杨庆庆 <972544908@qq.com>
2017-04-18 14:14:18 +0800

    first_cycleScroll_ok

--- DR_NewMedia/DR_NewMedia/YQCycleView.h
+++ DR_NewMedia/DR_NewMedia/YQCycleView.h
@@ -1,13 +1,13 @@
 //
-//  YQColletionView.h
+//  YQCycleView.h
 //  DR_NewMedia
 //
-//  Created by 杨庆 on 2017/4/17.
+//  Created by 杨庆 on 2017/4/18.
 //  Copyright © 2017年 YQ. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface YQColletionView : UICollectionView
+@interface YQCycleView : UIView
 
 @end
